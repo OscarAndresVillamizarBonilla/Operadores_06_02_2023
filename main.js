63d399e753b46e3bc9c310069be2080281624595
OscarAndresVillamizarBonilla <andrex.villamizar1@gmail.com>
2023-02-06 19:20:05 -0500

    operadores arimeticos

---
+++ main.js
@@ -0,0 +1,22 @@
+let num1 = 50;
+let num2 = 2;
+let solucion;
+//operador division
+solucion = num1 / num2;
+console.log(`${solucion}`);
+
+//operador multipiciacion
+solucion = num1 * num2;
+console.log(`${solucion}`);
+
+//operador porcentaje
+solucion = num1 % num2;
+console.log(`${solucion}`);
+
+//operador exponencial
+solucion = num1 ** num2;
+console.log(`${solucion}`);
+
+//operador unario positivo
+solucion
+
