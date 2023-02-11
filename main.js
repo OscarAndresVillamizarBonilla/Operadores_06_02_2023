096c7b9f81257067acc23488741ee5cee66c9d53
OscarAndresVillamizarBonilla <andrex.villamizar1@gmail.com>
2023-02-10 20:59:39 -0500

    Operador logico OR

--- main.js
+++ main.js
@@ -1,43 +1,9 @@
-let num1= 10, num2 = 80;
-let resultado;
-//equality
-resultado = num1==num2;
-console.log(`${num1} es igual a ${num2}?
-R: ${resultado}`);
+let a = confirm("Esta es la variable A\n\t\tPulsa Aceptar si quieres que sea un Boleano Verdadero(True)\n\t\tPulsa Cancelar si quieres que sea un boleano Falso(False)")
+let b = confirm("Esta es la variable B\n\t\tPulsa Aceptar si quieres que sea un Boleano Verdadero(True)\n\t\tPulsa Cancelar si quieres que sea un boleano Falso(False)")
 
-//ineqyality
-resultado = num1 != num2;
-console.log(`${num1} es desigual a ${num2}?
-R: ${resultado}`);
-
-//identity
-let num3 = 40;
-let num4 = 40;
-resultado = num3 === num4;
-console.log(`${num3} es identico a ${num4}? 
-R: ${resultado}`);
-
-// non-identity
-resultado = num3 !== num4;
-console.log(`${num1} no es identico a ${num2}?
-R: ${resultado}`);
-
-// greater than
-resultado = num1>num4;
-console.log(`${num1} es mayor que ${num4}?
-R: ${resultado}`);
-
-// geater than or equal
-resultado = num2 >= num4;
-console.log(`${num2} es igual o mayor que ${num4}
-R: ${resultado}`);
-
-// less than
-resultado = num3<num1;
-console.log(`${num3} es menor que ${num1}?
-R: ${resultado}`);
-
-// less than or equal
-resultado = num2 <= num1;
-console.log(`${num2} es menor o igual que ${num1}?
-R: ${resultado}`);
+let cuerpo = document.querySelector("#myAND");
+cuerpo.insertAdjacentHTML("beforeend", ` <tr>
+                                            <td>${a}</td>
+                                            <td>${b}</td>
+                                            <td>${a || b}</td>
+                                        </tr>`);
